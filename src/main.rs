5776be45564c2998221bd9755e2b4b1f871796f6
samarqandi <san4o.original@gmail.com>
2023-06-07 06:24:36 +0500

    add actix-web dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-fn main() {
-    print_hello(String::from("Sanjar"));
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
 }
 
-fn print_hello(name: String) {
-    println!("Hello, {name}");
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
